cc372387d222da8a3e37250dea69986913fa4bfa
mustafaberk01 <mustafaberkay5234@gmail.com>
2020-12-03 19:47:57 +0100

    initialize

---
+++ ValueConverterDersi/ValueConverterDersi/ValueConverterDersi/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ValueConverterDersi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+    public class BoolToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isCheckedProp = (bool)value;
+            if (isCheckedProp)
+            {
+                return Color.Black;
+            }
+            else
+            {
+                return Color.LightGray;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
