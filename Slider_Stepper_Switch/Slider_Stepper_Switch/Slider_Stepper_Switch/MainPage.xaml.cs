2cb0f29ace3cf4d255c097b7b6bffadcab1871da
mustafaberk01 <mustafaberkay5234@gmail.com>
2020-12-11 21:13:05 +0300

    intialize

---
+++ Slider_Stepper_Switch/Slider_Stepper_Switch/Slider_Stepper_Switch/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Slider_Stepper_Switch
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void swtch1_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (swtch1.IsToggled)
+            {
+                label.Text = "Acik";
+            }
+            else
+            {
+                label.Text = "Kapali";
+            }
+        }
+    }
+}
