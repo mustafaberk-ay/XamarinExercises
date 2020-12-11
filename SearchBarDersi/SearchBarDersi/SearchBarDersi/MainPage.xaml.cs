dbf447ec6ca4804bc102dfd8b2b052ca3bedba6b
mustafaberk01 <mustafaberkay5234@gmail.com>
2020-12-11 17:43:18 +0300

    initialize

---
+++ SearchBarDersi/SearchBarDersi/SearchBarDersi/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SearchBarDersi
+{
+    public partial class MainPage : ContentPage
+    {
+        //private readonly List<string> _names = new List<string>
+        //{
+        //    "Ahmet", "Murat", "Necati", "Ayse", "Fatma", "Cenk", "Burak"
+        //};
+        public MainPage()
+        {
+            InitializeComponent();
+            //lstPeople.ItemsSource = _names;
+        }
+
+        //private void searchBar_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    var keyword = searchBar.Text;
+        //    //Where fonksiyonu bir kosul fonksiyonudur. Burada _names listesinde x oyle ki x'in kucuk harflisi keyword'u icinde bulundurup bulundurmadigini kontrol eder.
+        //    var result = _names.Where(x => x.ToLower().Contains(keyword));
+        //    lstPeople.ItemsSource = result;
+        //}
+
+    }
+}
---
+++ SearchBarDersi/SearchBarDersi/SearchBarDersi/PersonViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SearchBarDersi
+{
+    public class PersonViewModel
+    {
+        public string Keyword { get; set; }
+        public ICommand SearchCommand { get; set; }
+        //public List<string> Results { get; set; }
+        public ObservableCollection<string> Names { get; set; }
+
+
+        public PersonViewModel()
+        {
+            SearchCommand = new Command(SearchAction);
+            //Results = new List<string>();
+            Names = new ObservableCollection<string>
+            {
+                "Ahmet", "Murat", "Necati", "Ayse", "Fatma", "Cenk", "Burak"
+            };
+        }
+
+        private void SearchAction(object obj)
+        {
+            if(!string.IsNullOrEmpty(Keyword))
+            {
+                var result = Names.Where(x => x.ToLower().Contains(Keyword)).ToList();
+                Names = new ObservableCollection<string>(result);
+            }
+        }
+    }
+}
