07b73197056d3cb06aacd75b5218048af2c8811a
mustafaberk01 <mustafaberkay5234@gmail.com>
2020-12-17 20:35:26 +0300

    initialize

---
+++ MailGondermeApp/MailGondermeApp/MailGondermeApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MailGondermeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> Recipients = new List<string>();
+                Recipients.Add(entryAdres.Text);
+                var message = new EmailMessage
+                {
+                    Subject = entrySubject.Text,
+                    Body = editorMessage.Text,
+                    To = Recipients,
+                    //Cc = ccRecipients,
+                    //Bcc = bccRecipients
+                };
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                // Email is not supported on this device
+            }
+            catch (Exception ex)
+            {
+                // Some other exception occurred
+            }
+        }
+    }
+}
