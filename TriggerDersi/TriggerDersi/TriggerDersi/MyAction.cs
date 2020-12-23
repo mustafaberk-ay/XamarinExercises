329858925a329ac94d2025cb247bccced97b7992
mustafaberk01 <mustafaberkay5234@gmail.com>
2020-12-23 21:10:27 +0300

    initialize

---
+++ TriggerDersi/TriggerDersi/TriggerDersi/MyAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TriggerDersi
+{
+    public class MyAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            if (!string.IsNullOrEmpty(sender.Text))
+            {
+                sender.BackgroundColor = Color.Orange;
+                sender.TextColor = Color.White;
+                sender.FontSize = 35;
+            }
+            else
+            {
+                sender.BackgroundColor = Color.White;
+                sender.TextColor = Color.Black;
+                sender.FontSize = 20;
+            }
+        }
+    }
+}
