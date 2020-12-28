8ca0f60742d750c1f1a7baef3fd1e5422adbc046
mustafaberk01 <mustafaberkay5234@gmail.com>
2020-12-28 22:52:27 +0300

    initialize

---
+++ BindableDersi/BindableDersi/BindableDersi/CustomControls/MyLabel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BindableDersi.CustomControls
+{
+    public class MyLabel : ContentView
+    {
+        Image image;
+        public MyLabel()
+        {
+            image = new Image();
+            image.WidthRequest = 300;
+            image.HeightRequest = 300;
+            StackLayout stackLayout = new StackLayout();
+            stackLayout.Children.Add(image);
+            this.Content = stackLayout;
+        }
+        public BindableProperty ImageProperty = BindableProperty.Create("Image", typeof(string), typeof(MyLabel), null, propertyChanged:OnPropertyChanged);
+
+        public string Image
+        {
+            get
+            {
+                return GetValue(ImageProperty).ToString();
+            }
+            set
+            {
+                SetValue(ImageProperty, value);
+            }
+        }
+        private static void OnPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (MyLabel)bindable;
+            control.image.Source = newValue.ToString();
+        }
+    }
+}
